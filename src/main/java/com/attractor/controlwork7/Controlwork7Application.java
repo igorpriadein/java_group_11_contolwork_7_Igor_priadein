7a0ca4d42fabdefed9f2f59c45f9a8f76d5949c9
IgorPriadein <iga2456@gmail.com>
2022-04-02 11:31:50 +0600

    Initial commit

---
+++ src/main/java/com/attractor/controlwork7/Controlwork7Application.java
@@ -0,0 +1,13 @@
+package com.attractor.controlwork7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Controlwork7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Controlwork7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/attractor/controlwork7/Controlwork7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attractor.controlwork7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Controlwork7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
